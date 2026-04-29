4fedb3a208348140ce4bd5ddb29f8f00e7bede13
Gonza <ruaguilar@frba.utn.edu.ar>
2026-04-29 17:19:29 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/Hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hola, mundo!\n"
+                 "Nombre y apellido: Ruben Gonzalo Aguilar\n"
+                 "Curso de AED: K1053\n"
+                 "Grupo asignado: 8\n";
+          
+}
